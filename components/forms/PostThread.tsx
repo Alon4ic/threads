5e35fe8e30c88b95b8d5c2f76e83968e7c8de2ef
Alon4ic <o.redjko@gmail.com>
2023-10-21 16:57:08 +0300

    error correction

--- components/forms/PostThread.tsx
+++ components/forms/PostThread.tsx
@@ -20,7 +20,6 @@ import { Textarea } from "@/components/ui/textarea";
 import { ThreadValidation } from "@/lib/validations/thread";
 import { createThread } from "@/lib/actions/thread.actions";
 
-
 interface Props {
 	userId: string;
 }
@@ -29,26 +28,29 @@ function PostThread({ userId }: Props) {
 	const router = useRouter();
 	const pathname = usePathname();
 
-	const form = useForm({
+	const { organization } = useOrganization();
+
+	const form = useForm<z.infer<typeof ThreadValidation>>({
 		resolver: zodResolver(ThreadValidation),
 		defaultValues: {
-			thread: '',
+			thread: "",
 			accountId: userId,
-		}
-	})
+		},
+	});
 
 	const onSubmit = async (values: z.infer<typeof ThreadValidation>) => {
 		await createThread({
 			text: values.thread,
-			 author: userId,
-			communityId: null, 
-			path: pathname
+			author: userId,
+			communityId: organization ? organization.id : null,
+			path: pathname,
 		});
-		router.push("/")
-	}
+
+		router.push("/");
+	};
 
 	return (
-		<Form {...form }>
+		<Form {...form}>
 			<form
 				className='mt-10 flex flex-col justify-start gap-10'
 				onSubmit={form.handleSubmit(onSubmit)}
@@ -61,22 +63,20 @@ function PostThread({ userId }: Props) {
 							<FormLabel className='text-base-semibold text-light-2'>
 								Content
 							</FormLabel>
-							<FormControl className="no-focus border border-dark-4 bg-dark-3 text-light-1">
-								<Textarea
-									rows={15}
-									{...field}
-								/>
+							<FormControl className='no-focus border border-dark-4 bg-dark-3 text-light-1'>
+								<Textarea rows={15} {...field} />
 							</FormControl>
 							<FormMessage />
 						</FormItem>
 					)}
 				/>
-				<Button type="submit" className="bg-primary-500">
+
+				<Button type='submit' className='bg-primary-500'>
 					Post Thread
 				</Button>
 			</form>
-		</Form >
-	)
+		</Form>
+	);
 }
 
 export default PostThread;
